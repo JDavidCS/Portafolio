598fcfeeef0755b1d8e2ce7ca2f55fd0e1108c77
JDavidCS <jdcorreasierra@gmail.com>
2023-07-17 18:11:56 -0500

    build: responsive project slider.

--- mainPage/js/slider.js
+++ mainPage/js/slider.js
@@ -16,10 +16,67 @@ const ListInput = document.querySelectorAll("[name=radio-btn]");
 const ListSlide = document.querySelectorAll(".card");
 const $slider = document.querySelector(".slider");
 
+const $next = document.querySelector('#btn-next');
+const $previous = document.querySelector('#btn-previous');
+
+let $checkedIndex = 0;
 let $checkedItem = updateChecked();
 
-$slider.addEventListener('change', (ev)=>{
+$slider.addEventListener('change', ()=>{
 
+    updateAnimation();
+    // const $card = document.querySelector(`#slide${$checkedItem.dataset.number}`);
+
+    // $card.classList.add('collapsed');
+    // $card.addEventListener('transitionend', function() {
+    //     $card.classList.remove('collapsed');
+    // });
+    // $checkedItem = updateChecked();
+});
+
+$next.addEventListener('click', ()=>{
+
+    if(ListInput.length > $checkedIndex+1){
+        ListInput[$checkedIndex+1].checked = true;
+    }else{
+        $checkedIndex = 0;
+        ListInput[$checkedIndex].checked = true;
+    }
+
+    updateAnimation();
+    // const $card = document.querySelector(`#slide${$checkedItem.dataset.number}`);
+
+    // $card.classList.add('collapsed');
+    // $card.addEventListener('transitionend', function() {
+    //     $card.classList.remove('collapsed');
+    // });
+    // $checkedItem = updateChecked();
+});
+
+$previous.addEventListener('click', ()=>{
+
+    if($checkedIndex != 0){
+            ListInput[$checkedIndex-1].checked = true;
+    }else{
+        $checkedIndex = ListInput.length-1;
+        ListInput[$checkedIndex].checked = true;
+    }
+
+    updateAnimation();
+});
+
+function updateChecked(){
+    let checked = null;
+    ListInput.forEach((el, key) =>{
+        if(el.checked){
+            checked = el;
+            $checkedIndex = key;
+        }
+    })
+    return checked;
+}
+
+function updateAnimation(){
     const $card = document.querySelector(`#slide${$checkedItem.dataset.number}`);
 
     $card.classList.add('collapsed');
@@ -27,14 +84,4 @@ $slider.addEventListener('change', (ev)=>{
         $card.classList.remove('collapsed');
     });
     $checkedItem = updateChecked();
-});
-
-function updateChecked(){
-    let checked = null;
-    ListInput.forEach(el =>{
-        if(el.checked){
-            checked = el
-        }
-    })
-    return checked;
 }
